2ada4241e16ed11320c1c6c42c5c4467e1ca2883
prasetyanurangga <angganurprasetya4@gmail.com>
2022-08-03 20:03:07 +0700

    add some plugin

--- gulpfile.js
+++ gulpfile.js
@@ -10,22 +10,27 @@ var path = {
     },
     src: {
         html: 'assets/src/*.html',
-        js: 'assets/src/js/*.js',
-        css: 'assets/src/style/css/*.css',
+        js: 'assets/src/js/!(*.min)*.js',
+        min_js: 'assets/src/js/*.min.js',
+        css: 'assets/src/style/css/!(*.min)*.css',
+        min_css: 'assets/src/style/css/*.min.css',
         scss: 'assets/src/style/scss/*.scss',
         img: 'assets/src/img/**/*.*'
     },
     watch: {
         html: 'assets/src/**/*.html',
-        js: 'assets/src/js/**/*.js',
+        js: 'assets/src/js/**/!(*.min)*.js',
+        min_js: 'assets/src/js/**/*.min.js',
         scss: 'assets/src/style/scss/**/*.scss',
-        css: 'assets/src/style/css/**/*.css',
+        css: 'assets/src/style/css/**/!(*.min)*.css',
+        min_css: 'assets/src/style/css/**/*.min.css',
         img: 'assets/src/img/**/*.*'
     },
     clean: 'assets/build/*'
 };
 
 var config = {
+    port: 2001,
     server: {
         baseDir: './assets/build'
     },
@@ -41,11 +46,12 @@ var gulp = require('gulp'),  // include Gulp
     cleanCSS = require('gulp-clean-css'), // CSS minification plugin
     uglify = require('gulp-uglify'), // JavaScript minification module
     clean = require('gulp-clean'), // plugin for deleting files and directories
-    rename = require('gulp-rename'),
+    rename = require('gulp-rename'), // plugin for rename file
     autoprefixer = require('autoprefixer'),
     gulpAutoprefixer = require('gulp-autoprefixer'),
     tailwind = require('tailwindcss'),
     postcss = require('gulp-postcss'),
+    formatHtml = require('gulp-format-html'), // plugin for format file html
     logger = require('pino')()
 
 logger.info('Gulp and pino are working...');
@@ -63,6 +69,7 @@ gulp.task('html:build', function () {
     return gulp.src(path.src.html) // selection of all html files in the specified path
         .pipe(plumber()) // error tracking
         .pipe(rigger()) // attachment import
+        .pipe(formatHtml())
         .pipe(gulp.dest(path.build.html)) // uploading ready files
         .pipe(webserver.reload({ stream: true })); // server reboot
 });
@@ -72,7 +79,6 @@ gulp.task('css:build', function () {
     return gulp.src(path.src.css) // get main.scss
         .pipe(plumber()) // for bug tracking
         .pipe(gulpAutoprefixer()) // add prefix
-        .pipe(gulp.dest(path.build.css))
         .pipe(rename({ suffix: '.min' }))
         .pipe(cleanCSS()) // minimize CSS
         .pipe(gulp.dest(path.build.css)) // output to build
@@ -88,7 +94,6 @@ gulp.task('scss:build', function () {
           tailwind,
           autoprefixer
         ]))
-        .pipe(gulp.dest(path.build.css))
         .pipe(rename({ suffix: '.min' }))
         .pipe(cleanCSS()) // minimize CSS
         .pipe(gulp.dest(path.build.css)) // output to build
@@ -106,12 +111,26 @@ gulp.task('js:build', function () {
         .pipe(webserver.reload({ stream: true })); // server restart
 });
 
+// compile js
+gulp.task('min_js:build', function () {
+    return gulp.src(path.src.min_js) // get file main.js
+        .pipe(gulp.dest(path.build.js)) // put ready file
+        .pipe(webserver.reload({ stream: true })); // server restart
+});
+
+gulp.task('min_css:build', function () {
+    return gulp.src(path.src.min_css) // get file main.js
+        .pipe(gulp.dest(path.build.css)) // put ready file
+        .pipe(webserver.reload({ stream: true })); // server restart
+});
+
 
 
 // image processing
 gulp.task('image:build', function () {
     return gulp.src(path.src.img) // path to image source=
-        .pipe(gulp.dest(path.build.img)); // output ready files
+        .pipe(gulp.dest(path.build.img)) // output ready files
+        .pipe(webserver.reload({ stream: true })); // server restart
 });
 
 // remove catalog build
@@ -132,8 +151,10 @@ gulp.task('build',
         gulp.parallel(
             'html:build',
             'css:build',
+            'min_css:build',
             'scss:build',
             'js:build',
+            'min_js:build',
             'image:build'
         )
     )
@@ -145,8 +166,10 @@ gulp.task('build',
 gulp.task('watch', function () {
     gulp.watch(path.watch.html, gulp.series('html:build', 'scss:build', 'css:build' ));
     gulp.watch(path.watch.css, gulp.series('css:build'));
+    gulp.watch(path.watch.min_css, gulp.series('min_css:build'));
     gulp.watch(path.watch.scss, gulp.series('scss:build'));
     gulp.watch(path.watch.js, gulp.series('js:build'));
+    gulp.watch(path.watch.min_js, gulp.series('min_js:build'));
     gulp.watch(path.watch.img, gulp.series('image:build'));
 });
 
